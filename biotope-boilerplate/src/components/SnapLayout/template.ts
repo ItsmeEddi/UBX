3a10e836aa168e59dc60318cdda62a5c6340ee4e
Edwin Repp <edwin.repp@virtual-identity.com>
2019-04-17 15:50:06 +0200

    added grain animation

--- biotope-boilerplate/src/components/SnapLayout/template.ts
+++ biotope-boilerplate/src/components/SnapLayout/template.ts
@@ -8,15 +8,15 @@ export default (render: Function, data: SnapLayoutTemplateData) => {
     return render`
         <style>${styles.toString()}</style>
         <div class="snap-layout__container snap-layout__container--vertical">
-            <div class="snap-layout__section section-1">Item 1</div>
-            <div class="snap-layout__section section-2">
+            <div class="snap-layout__section dark-blue" >Item 1</div>
+            <div class="snap-layout__section navi-blue">
                 <div class="snap-layout__container">
-                    <div class="snap-layout__section section-3">Item 2.1</div>
-                    <div class="snap-layout__section section-4">Item 2.2</div>
-                    <div class="snap-layout__section section-5">Item 2.3</div>
+                    <div class="snap-layout__section navi-blue">Item 2.1</div>
+                    <div class="snap-layout__section navi-blue">Item 2.2</div>
+                    <div class="snap-layout__section navi-blue">Item 2.3</div>
                 </div>
             </div>
-        <div class="snap-layout__section section-6">Item 3</div>
+        <div class="snap-layout__section dark-blue">Item 3</div>
         </div>
     `;
 }
