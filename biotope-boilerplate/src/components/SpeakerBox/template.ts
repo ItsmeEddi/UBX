f91caef6f1db34ac701843b42d7186c8d9bd07b1
Edwin Repp <edwin.repp@virtual-identity.com>
2019-05-02 15:58:39 +0200

    add styles for speaker component

--- biotope-boilerplate/src/components/SpeakerBox/template.ts
+++ biotope-boilerplate/src/components/SpeakerBox/template.ts
@@ -15,7 +15,7 @@ export default (render: Function, data: SpeakerBoxTemplateData) => {
             <div class="speaker__box-two"></div>
             <h3 class="speaker__title">Tom De Bruyne</h3>
             <span class="speaker__job">Founder & Creative Director<br>SUE Behavioural Design</span>
-            <i class="speaker__toggle" onclick="${data.toggleOpen}"></i>
+            <p class="speaker__toggle" onclick="${data.toggleOpen}">more information<i class="speaker__toggle--arrow"></i></p>
             <p class="speaker__description">Test</p>
         </div>
     `;
