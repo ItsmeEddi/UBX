2c30867e315d02b5500b8214f46c08dc84d15e71
Edwin Repp <edwin.repp@virtual-identity.com>
2019-05-03 19:18:13 +0200

    add fluid styles to speakerBox component

--- biotope-boilerplate/src/components/SpeakerBox/template.ts
+++ biotope-boilerplate/src/components/SpeakerBox/template.ts
@@ -10,15 +10,23 @@ interface SpeakerBoxTemplateData {
 }
 
 export default (render: Function, data: SpeakerBoxTemplateData) => {
+    const dynamicStyles = `
+        :host .speaker__box-one::after {
+            background-image: url(${data.image});
+        }
+    `;
     return render`
         <style>${styles.toString()}</style>
+        <style>
+            ${dynamicStyles}
+        </style> 
         <div class="speaker">
-            <img src=${data.image}>
-            <div class="speaker__box-one"></div>
-            <div class="speaker__box-two"></div>
-            <h3 class="speaker__title">${data.firstName}<br>${data.lastName}</h3>
-            <span class="speaker__job">${data.jobInfo}</span>
-            <p class="speaker__toggle" onclick="${data.toggleOpen}">more information<i class="speaker__toggle--arrow"></i></p>
+            <div class="speaker__wrapper">
+                <div class="speaker__box-one"></div>
+                <h3 class="speaker__title">${data.firstName}<br>${data.lastName}</h3>
+                <span class="speaker__job">${data.jobInfo}</span>
+                <p class="speaker__toggle" onclick="${data.toggleOpen}">more information<i class="speaker__toggle--arrow"></i></p>
+            </div>
             <p class="speaker__description">${data.description}</p>
         </div>
     `;
