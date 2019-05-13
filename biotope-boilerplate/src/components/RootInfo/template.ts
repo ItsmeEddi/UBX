fd42eed0dac420bb378e469c8bcc318ec2ea054d
Edwin Repp <edwin.repp@virtual-identity.com>
2019-05-13 10:14:30 +0200

    text changes

--- biotope-boilerplate/src/components/RootInfo/template.ts
+++ biotope-boilerplate/src/components/RootInfo/template.ts
@@ -5,13 +5,13 @@ interface RootInfoTemplateData {
 }
 
 export default (render: Function, data: RootInfoTemplateData) => {
-    return render`
+  return render`
         <style>${styles.toString()}</style>
         <div class="info twelve-column-grid">
             <glyph-headline dark>UBX 2019</glyph-headline>
             <event-date></event-date>
             <simple-text>What role does Useful Brand Experience play in redefining digital business? Under the motto "Creating Digital Opportunities", UBX19 presents how the power and diversity of digital solutions and processes can be used to create rewarding brand experiences. UBX19 combines inspiring keynotes and innovative digitisation cases from all over the world.</simple-text>
-            <simple-button link="#registration">Registration</simple-button>
+            <simple-button link="#registration">Apply now</simple-button>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="200 250 800 800">
   <defs>
     <style>
--- biotope-boilerplate/src/components/RootRegistration/template.ts
+++ biotope-boilerplate/src/components/RootRegistration/template.ts
@@ -9,7 +9,7 @@ export default (render: Function, data: RootRegistrationTemplateData) => {
         <style>${styles.toString()}</style>
         <div class="registration twelve-column-grid">
             <glyph-headline light>Registration</glyph-headline>
-            <simple-text>Would you like to attend the UBX Conference on October 17?<br>Sign up by writing us why you want to join.</simple-text>
+            <simple-text>Would you like to attend the UBX Conference on October 17?<br>Apply now by writing us why you want to join.</simple-text>
             <registration-form></registration-form>
         </div>
     `;
