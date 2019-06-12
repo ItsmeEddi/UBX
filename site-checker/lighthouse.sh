9cbdb45da857d013032d7f2cc0a161e87cde6640
Jurek Barth <jurek.barth@virtual-identity.com>
2019-06-12 14:32:03 +0200

    change lighthouse invocation

--- site-checker/lighthouse.sh
+++ site-checker/lighthouse.sh
@@ -6,4 +6,4 @@ url="$value/konferenz/"
 
 echo "testing with url: $url"
 
-npm run lh --pwa=40 --perf=80 --seo=95 --a11y=100 --bp=90 $url
+./node_modules/.bin/lighthousebot --pwa=40 --perf=80 --seo=95 --a11y=100 --bp=90 $url
