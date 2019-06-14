4340557312dc5330132024d1b1a2250c85efa018
Edwin Repp <edwin.repp@virtual-identity.com>
2019-06-14 16:22:38 +0200

    add image to location section

--- biotope-boilerplate/src/components/RootLocation/template.ts
+++ biotope-boilerplate/src/components/RootLocation/template.ts
@@ -8,9 +8,14 @@ export default (render: Function, data: RootLocationTemplateData) => {
     return render`
         <style>${styles.toString()}</style>
         <div class="location twelve-column-grid">
-            <simple-headline light>Location</simple-headline>
-            <simple-text>The conference is hosted by Virtual Identity. For the fifth time the digital agency transform its Munich office into the conference venue.</simple-text>
-            <address-label></address-label>
+            <div class="location__text">
+                <simple-headline light>Location</simple-headline>
+                <simple-text>The conference is hosted by Virtual Identity. For the fifth time the digital agency transform its Munich office into the conference venue.</simple-text>
+                <address-label></address-label>
+            </div>
+            <div class="location__images">
+                <img  alt="Virtual Identity building" src="resources/img/ubx_vi-building.jpg">
+            </div>
         </div>
     `;
 }
