8ca56e1c1cab8a224684a83472fbf9adca8d0c1e
Danny Stey <danny.stey@virtual-identity.com>
2019-09-12 17:49:03 +0200

    changed head html

--- privacy-template.php
+++ privacy-template.php
@@ -12,73 +12,14 @@
 
 
 <head>
-	<meta charset="utf-8">
-
-<!-- Use the .htaccess and remove these lines to avoid edge case issues. More info: h5bp.com/b/378 -->
-<!-- DEV NOTE: whenever possible set the HTTP-Header for X-UA-Compatible too to avoid problems with intranet zones -->
-<meta http-equiv="x-ua-compatible" content="ie=edge">
-
-<title>UBX Conference: Creating Digital Opportunities</title>
-<link rel="shortcut icon" href="/konferenz/resources/img/favicon.ico" />
-<meta name="description"
-    content="The UBX conference on 17 October 2019 in Munich will show how the diversity of digital solutions can be used to create positive brand experiences.">
-
-<!-- Mobile viewport optimized: h5bp.com/viewport -->
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="msapplication-tap-highlight" content="no">
-<!-- do not detect phone numbers -->
-<meta name="format-detection" content="telephone=no">
-
-<meta property="og:title" content="UBX Konferenz: Creating Digital Opportunities">
-<meta property="og:description"
-    content="The UBX conference on 17 October 2019 in Munich will show how the diversity of digital solutions can be used to create positive brand experiences.">
-<meta property="og:type" content="website">
-
-<!--DEV-NOTE: next two exntries are optional. define a custom share image and URL or remove this two lines -->
-<meta property="og:url" content="">
-<meta property="og:image" content="/konferenz/resources/img/ubx_teaser.jpg">
-
-
-	<!-- inject:head:html -->
-	<!-- endinject -->
-
-	<!-- DEV-note: include page specific meta information after this comment -->
-
-	<!-- CSS: implied media=all -->
-<!-- CSS should be concatenated and minified in production use -->
-
-<!-- The following files are merged into resources/css/styles.all.min.css -->
-<link rel="stylesheet" href="/konferenz/resources/css/styles.all.min.css">
-
-<!-- The following files are merged into resources/css/styles.print.min.css -->
-<link rel="stylesheet" href="/konferenz/resources/css/styles.print.min.css" media="print">
-
-<style>
-  .grecaptcha-badge {
-      visibility: hidden;
-    }
-</style>
-
-
-	<!-- All JavaScript at the bottom, except for Modernizr and configuration class -->
-<script src="https://unpkg.com/@webcomponents/webcomponentsjs@2.2.1/webcomponents-loader.js"></script>
-<script nomodule src="https://unpkg.com/whatwg-fetch"></script>
-<script nomodule src="https://unpkg.com/promise-polyfill"></script>
-<script nomodule src="https://unpkg.com/object-assign-polyfill"></script>
-
-<script src="/konferenz/resources/js/scripts.head.all.min.js"></script>
-
-<!-- Site-specific configurations like CDN resource folder or current page ID -->
-<script type="text/javascript">
-	biotope.configuration.set('data.staticResourcesBase', '/konferenz/resources/');
-	biotope.configuration.set('data.staticResourcesContentRepoBase', '/konferenz/resources-content/');
-</script>
-
-
-	<!-- DEV-note: include page specific styles and scripts after this comment -->
+<?php include_once('./htmlhead.php'); ?>
 </head>
 
 <body role="document" class="">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVJN724"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 	<main class="content">
 		<article>
 			<page-layout headline="Data protection declaration / information according to art. 13 DSGVO" data-resources="[{paths: ['components/PageLayout/index.js']}]">
