1c093b26e5c00e538ba9833908c4b5a6a46b2d5f
Robin Spielmann <robin.spielmann@virtual-identity.com>
2019-10-10 15:45:18 +0200

    remove php files and change sync-sh logic

--- netlify.sh
+++ netlify.sh
@@ -8,5 +8,3 @@ node generateFiles.js
 
 cp -r ./biotope-boilerplate/dist/resources ./dist/konferenz/resources
 cp -r ./biotope-boilerplate/dist/_config ./dist/konferenz/_config
-
-
--- sync.sh
+++ sync.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
-cd ./biotope-boilerplate && npm install && npm run build
+#cd ./biotope-boilerplate && npm install && npm run build
 
-cd ..
+#cd ..
 
-node generateFiles.js
+#node generateFiles.js
 
-password=$(rclone obscure $1)
+#password=$(rclone obscure $1)
 
-rclone sync --sftp-pass $password --config ./rclone.conf ./biotope-boilerplate/dist/resources vi-ubx:/var/chroots/ubx/var/www/vhosts/konferenz/resources
+rm -f ./biotope-boilerplate/dist/index.html
 
-rclone copy --sftp-pass $password --config ./rclone.conf ./biotope-template.php vi-ubx:/var/chroots/ubx/var/www/vhosts/konferenz/wp-content/themes/ubxconference
+mv ./biotope-boilerplate/dist/11pages.Conference.html ./biotope-boilerplate/dist/index.html
 
-rclone copy --sftp-pass $password --config ./rclone.conf ./privacy-template.php vi-ubx:/var/chroots/ubx/var/www/vhosts/konferenz/wp-content/themes/ubxconference
+#push dist folder to /vhost/konferenz
+#rclone sync --sftp-pass $password --config ./rclone.conf ./biotope-boilerplate/dist vi-ubx:/var/chroots/ubx/var/www/vhosts/konferenz
 
-rclone copy --sftp-pass $password --config ./rclone.conf ./impress-template.php vi-ubx:/var/chroots/ubx/var/www/vhosts/konferenz/wp-content/themes/ubxconference
+#rclone copy --sftp-pass $password --config ./rclone.conf ./biotope-template.php vi-ubx:/var/chroots/ubx/var/www/vhosts/konferenz/wp-content/themes/ubxconference
 
-rclone copy --sftp-pass $password --config ./rclone.conf ./former-talks-template.php vi-ubx:/var/chroots/ubx/var/www/vhosts/konferenz/wp-content/themes/ubxconference
+#rclone copy --sftp-pass $password --config ./rclone.conf ./privacy-template.php vi-ubx:/var/chroots/ubx/var/www/vhosts/konferenz/wp-content/themes/ubxconference
+
+#rclone copy --sftp-pass $password --config ./rclone.conf ./impress-template.php vi-ubx:/var/chroots/ubx/var/www/vhosts/konferenz/wp-content/themes/ubxconference
+
+#rclone copy --sftp-pass $password --config ./rclone.conf ./former-talks-template.php vi-ubx:/var/chroots/ubx/var/www/vhosts/konferenz/wp-content/themes/ubxconference
