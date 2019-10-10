c875b3638ad880a23a997a0e391a2608f8e73e23
Vasco Costa <vasco.costa@virtual-identity.com>
2019-10-10 12:01:22 +0100

    Add blog speakers.

--- biotope-boilerplate/src/components/RootSpeakers/template.ts
+++ biotope-boilerplate/src/components/RootSpeakers/template.ts
@@ -21,6 +21,13 @@ const speakersData = [
         description: 'Dirk von Gehlen is an author, journalist, head of the Social Media/Innovation department at one of Germanys most important newspapers and has been researching the social consequences of digitisation for years. As the inventor of the "pragmatism principle" ¯\_(ツ)_/¯ he pleads for a culturally pragmatic approach to the new – which will be perfectly fitting into the spirit of UBX.'
     },
     {
+        firstName: "Cindy-Ricarda",
+        lastName: "Robert",
+        image: "resources/img/UBX19_RcardaRoberts.png",
+        jobInfo: "Scientist / PhD Student Technical University, Munich",
+        description: "Cindy-Ricarda Roberts is a research assistant at the Chair of Media Ethics at the Munich School of Philosophy. Currently she is completing her doctoral thesis on democracy and digitization. Her research topics include fake news, hacking and their consequences for democratic processes. At UBX she will participate in an expert panel on the balance of AI and ethics together with Dagmar Schuller and Christian Greb."
+    },
+    {
         firstName: "Sam",
         lastName: "Conniff Allende",
         image: "resources/img/UBX19_Conniff.png",
@@ -40,6 +47,13 @@ const speakersData = [
         image: "resources/img/UBX19_Onu.png",
         jobInfo: "Founder & AI Research Lead Ubenwa Intelligence Solutions",
         description: 'Charles C. Uno has founded a company that applies AI and mobile technology to enable timely diagnosis of birth asphyxia from a newborn cry. He has over six years experience in the development of machine learning software for real-world health care problems such as breast cancer patient care and intensive care unit (ICU) monitoring. At UBX, he will share his passion about developing high impact technologies that address social challenges, especially in resource-poor settings.'
+    },
+    {
+        firstName: "Anne-Kathrin",
+        lastName: "Bedoy",
+        image: "resources/img/UBX19_Bedoy.png",
+        jobInfo: "Digital & Communication Director Active Cosmetics, L'Oréal",
+        description: "Anne-Kathrin Bedoy heads the digital agenda of the dermocosmetics division at L'Oréal Germany. Together with Linda Schreurs, who is on the same mission for L'Oréal's skincare brand La Roche-Posay, she implements ‘My Skin Track UV', the first wearable sensor and companion app to determine personal UV exposure, pollution, pollen levels and humidity. At UBX, their tandem lecture will show how scientific and technological progress can take the user experience onto a new level."
     },
     {
         firstName: "Tom",
@@ -70,11 +84,32 @@ const speakersData = [
         description: 'Christian Greb explores and analyses linguistic and written communication by using AI. His credo: words transport more than pure content. By combining technology with human emotions, he identifies hidden personality traits and special abilities and potentials. At UBX, the co-founder of Precire shows how written and spoken language is decoded and broken down into tens of thousands of tiny digital building blocks and elements using software – practical approaches included.'
     },
     {
-        firstName: "Anne-Kathrin",
-        lastName: "Bedoy",
-        image: "resources/img/UBX19_Bedoy.png",
-        jobInfo: "Digital & Communication Director Active Cosmetics, L'Oréal",
-        description: "Anne-Kathrin Bedoy heads the digital agenda of the dermocosmetics division at L'Oréal Germany. Together with Linda Schreurs, who is on the same mission for L'Oréal's skincare brand La Roche-Posay, she implements ‘My Skin Track UV', the first wearable sensor and companion app to determine personal UV exposure, pollution, pollen levels and humidity. At UBX, their tandem lecture will show how scientific and technological progress can take the user experience onto a new level."
+        firstName: "Dagmar",
+        lastName: "Schuller",
+        image: "resources/img/UBX19_Schuller.png",
+        jobInfo: "CEO & Co-Founder audEERING, Munich/Berlin",
+        description: "Dagmar Schuller is among the most important experts in (emotional) artificial intelligence, machine learning and big data. Developing and working on it for over ten years, with a clear focus on the benefits of intelligent audio analysis for the people. With her team at the AI innovator audEERING, she is thus developing new applications for a wide range of fields, from medicine and automotive to smart personal assistants. At UBX she is part of a top-class expert panel on AI and ethics."
+    },
+    {
+        firstName: "Dennis",
+        lastName: "Maier",
+        image: "resources/img/UBX19_Meier.png",
+        jobInfo: "AI Researcher & Software Developer Cairo AG, Mannheim",
+        description: "Dennis Maier takes the technical lead in software development and AI research at Cairo AG. He focuses on data science, machine learning, image classification & object detection to open up new solutions in the field of AI and the IIoT. He was also nominated for this year' s Digital Leader's Award and will participate in the discussion round \"How to become a Digital Leader\" at UBX, which will be moderated by IDG."
+    },
+    {
+        firstName: "Thomas",
+        lastName: "Henzler",
+        image: "resources/img/UBX19_Henzler.png",
+        jobInfo: "Chief Information Officer Piller Blowers & Compressors, Moringen",
+        description: "Thomas Henzel is CIO of PB&C, a well-known manufacturer of high-power blowers and compressors for the process industry. Directing the headquarter IT as well as strategic business projects and operating as a solution architect for SAP modules. This year he was a nominee for the Digital Leader's Award. At UBX, he will take part in a panel of digitization executives trying to figure out how to become a digital leader."
+    },
+    {
+        firstName: "Uwe",
+        lastName: "Linck",
+        image: "resources/img/UBX19_Linck.png",
+        jobInfo: "Head of Test Department & IT Administration Lorenz GmbH & Co. KG, Schelklingen-Ingstetten",
+        description: "Uwe Linck is specialized in flow measurement at Lorenz GmbH & Co. KG. Apart from developing new radio integrated digital water meters, he and his team digitize production facilities and manufacturing processes – and thereby transform a traditional company into a digitally driven manufacturer of cutting-edge technology. Nominated for this year's Digital Leader's Award, he will be part of the panel discussion \"How to become a Digital Leader\" at UBX."
     },
     {
         firstName: "Linda",
